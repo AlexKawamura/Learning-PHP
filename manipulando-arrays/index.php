8da71d7033fb18ec0086dd0ac54c22da1eb71400
Alexandre Kawamura Tanaka <alexandretanaka@alunos.utfpr.edu.br>
2019-07-30 08:58:12 -0300

    Ordenando, explode e implode arrays

--- manipulando-arrays/index.php
+++ manipulando-arrays/index.php
@@ -1,8 +1,10 @@
 <?php
 
+echo "<h2>CALCULADORA DE MÉDIA</h2>";
+
 require_once "Calculadora.php";
 
-$notas = [9, 5, 10, 3];
+$notas = [];
 
 $calculadora = new Calculadora();
 $media = $calculadora->calculaMedia($notas);
@@ -12,3 +14,30 @@ if ($media) {
 } else {
     echo "Não foi possível calcular a média";
 }
+/*-----------------------------------------------------------------------------------*/
+echo "<h2>ORDENANDO SALDOS</h2>";
+$saldos = [2500, 3000, 1000, 8700, 9000];
+
+foreach ($saldos as $saldo) {
+    echo "<p>O saldo é $saldo</p>";
+}
+
+/* sort() -> Faz ordenação do array de forma crescente */
+sort($saldos);
+
+echo "O menor saldo é $saldos[0]";
+/*-----------------------------------------------------------------------------------*/
+echo "<h2>SEPARANDO E JUNTANDO NOMES</h2>";
+$nomes = "Giovanni, João, Maria, Pedro";
+
+/* explode() -> Separa a string em um array de acordo com o delimitador */
+$arrayNomes = explode(", ", $nomes);
+
+foreach ($arrayNomes as $nome) {
+    echo "<p>Olá $nome</p>";
+}
+
+/* implode() -> Junta os arrays em uma string */
+$stringNomes = implode(", ", $arrayNomes);
+
+echo "$stringNomes";
